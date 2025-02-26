5cab6016568d8274c6ef2d97e01a5351dca8e476
Heikki Lehtosalo <heikki.lehtosalo@gmail.com>
2025-02-26 22:38:17 +0200

    Remove ; to : mapping, to use ; for repeating f and t motions

--- init.lua
+++ init.lua
@@ -159,8 +159,6 @@ vim.opt.scrolloff = 10
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
-vim.keymap.set('n', ';', ':')
-
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
